37a8f21c6eb0c7ec12f3acf66577e55690571a7c
Star-mi <star-mi-7@mail.ru>
2023-03-06 16:09:11 +0200

    refactor exceptions

--- src/main/java/com/hackaton/toncash/exception/OrderNotFoundException.java
+++ src/main/java/com/hackaton/toncash/exception/OrderNotFoundException.java
@@ -3,7 +3,6 @@ package com.hackaton.toncash.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.Serial;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class OrderNotFoundException extends RuntimeException {
@@ -11,7 +10,6 @@ public class OrderNotFoundException extends RuntimeException {
 	/**
 	 *
 	 */
-	@Serial
 	private static final long serialVersionUID = -8393111568194409951L;
 
 	public OrderNotFoundException(String id) {
