e84e9faef1b1b68de9eec9302891616e9a66b097
Star-mi <star-mi-7@mail.ru>
2023-03-07 21:03:00 +0200

    refactor cors

--- src/main/java/com/hackaton/toncash/controller/OrderController.java
+++ src/main/java/com/hackaton/toncash/controller/OrderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/orders")
-@CrossOrigin(origins = "/**", allowedHeaders = "/**")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @AllArgsConstructor
 public class OrderController {
     private final OrderService orderService;
--- src/main/java/com/hackaton/toncash/controller/PersonController.java
+++ src/main/java/com/hackaton/toncash/controller/PersonController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/persons")
-@CrossOrigin(origins = "/**", allowedHeaders = "/**")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @AllArgsConstructor
 public class PersonController {
     private final PersonService personService;
